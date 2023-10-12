f5b687d140d550f3cbbc41481720bdf4c4149d39
NAdamo <nadamo86@gmail.com>
2023-10-12 21:11:15 +0000

    feat: Add filter zone
    Filter zone created, Layout modification in the App.tsx
    
    Resolves #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { Container, Grid } from "@mui/material"
 import CountryList from "./components/CountryList"
+import FilterZone from "./components/filters/FilterZone"
 
 function App() {
 
   return (
-    <>
-      <CountryList />
-    </>
+    <Container sx={{ padding: 2 }}>
+      <Grid container spacing={4}>
+        <Grid item xs={3}>
+          <FilterZone />
+        </Grid>
+        <Grid item xs={9}>
+          <CountryList />
+        </Grid>
+      </Grid>
+    </Container >
   )
 }
 
---
+++ src/components/filters/FilterZone.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from "react";
+import StyledHeader from "../StyledHeader";
+
+const FilterZone: FunctionComponent = () => {
+    return (
+        <>
+            <StyledHeader>Filter</StyledHeader>
+        </>
+    );
+};
+
+export default FilterZone;
